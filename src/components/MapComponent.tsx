11f693f661c55883edc530008bc4e959ff34bbc9
wesscheid <wesscheid@gmail.com>
2026-01-16 01:09:22 -0600

    feat: switch map to dark mode using Carto Dark Matter tiles

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -56,8 +56,8 @@ const MapComponent: React.FC = () => {
         zoomControl={false}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
         />
         
         <MapUpdater />
